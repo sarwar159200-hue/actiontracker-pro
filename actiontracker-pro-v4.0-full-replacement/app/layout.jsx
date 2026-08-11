3143dc915f04dc883452da35f288416391e2fe78
sarwar159200-hue <sarwar.159200@gmail.com>
2026-08-11 11:56:17 +0300

    Add files via upload

---
+++ actiontracker-pro-v4.0-full-replacement/app/layout.jsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Action Tracker Pro | Miran Energy"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
