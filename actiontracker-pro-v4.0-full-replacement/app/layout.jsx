52e42973136b9035ac3caaa0e243350d73532e5e
sarwar159200-hue <sarwar.159200@gmail.com>
2026-08-11 22:36:34 +0300

    Delete actiontracker-pro-v4.0-full-replacement directory

--- actiontracker-pro-v4.0-full-replacement/app/layout.jsx
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"Action Tracker Pro | Miran Energy"};
-export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
